4d804dc434e1f4822311ea21d041dd4a81b942d4
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-09 13:39:34 -0500

    finish MatMat, add docs

--- hw1/src/mat_mat_multiply.c
+++ hw1/src/mat_mat_multiply.c
@@ -1,12 +1,27 @@
 #include "mat_mat_multiply.h"
 
 double* MatMat(const double* A, const size_t A_rows, const size_t A_cols,
-               const double* B, const size_t B_rows, const size_t B_cols) {}
+               const double* B, const size_t B_rows, const size_t B_cols) {
+  double* result = malloc(A_rows * B_cols * sizeof(double));
+  for (size_t i = 0; i < A_rows; i++) {
+    for (size_t j = 0; j < B_cols; j++) {
+      size_t results_flat_index = ComputeFlatIndex(B_cols, i, j);
+      result[results_flat_index] = MatMatEntry(A, A_cols, B, B_cols, i, j);
+    }
+  }
+  return result;
+}
 
-void SetMatEntry(double* Mat, const size_t n_cols, const size_t row,
-                 const size_t col, const double val) {
-  size_t flat_index = ComputeFlatIndex(n_cols, row, col);
-  Mat[flat_index] = val;
+double MatMatEntry(const double* A, const size_t A_cols, const double* B,
+                   const size_t B_cols, const size_t row, const size_t col) {
+  size_t K = A_cols;
+  double sum = 0.0;
+  for (size_t k = 0; k < K - 1; k++) {
+    size_t A_flat_index = ComputeFlatIndex(A_cols, row, k);
+    size_t B_flat_index = ComputeFlatIndex(B_cols, k, col);
+    sum += (A[A_flat_index] * B[B_flat_index]);
+  }
+  return sum;
 }
 
 size_t ComputeFlatIndex(const size_t n_cols, const size_t row,
