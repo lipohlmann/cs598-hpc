084d463e04e982fd020b1f2d30510c6dff9db40e
Liam Pohlmann <liam.pohlmann@gmail.com>
2026-09-10 13:56:47 -0500

    rename directory, add results

--- hw1/matlab_results/batch_octave.sh
+++ hw1/matlab_results/batch_octave.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH --account=26fa-cs598pa-eng     # This is valid for all students
 #SBATCH --partition=eng-instruction    # <- replace with your partition
-#SBATCH --time=00:10:00
+#SBATCH --time=01:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
+#SBATCH --mem=16G
 #SBATCH --job-name=run_octave
 #SBATCH --output=logfile
 
